7cbc37593c29260e0c30efe2e427afc86c9e3d7e
dlin04 <117554779+dlin04@users.noreply.github.com>
2022-11-06 01:38:09 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -2,7 +2,9 @@
 
 import wolframalpha
 
-question = input('Question: ')
+file = open('question.txt', 'r')  # the question will be saved to a text filed called 'question.txt'
+question = file.readline()  # assuming question will saved to a single line
+
 app_id = 'AEYTEG-EQ5R5LXU2P'
 client = wolframalpha.Client(app_id)
 res = client.query(question)
